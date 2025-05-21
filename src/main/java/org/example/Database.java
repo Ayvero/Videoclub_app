50c449433a1d7e70e88b8b387a5f9b5dda13e07a
Analia Burgos <analiaveronicadra@gmail.com>
2025-05-21 19:52:19 -0300

    cambio base de datos a supabase

--- src/main/java/org/example/Database.java
+++ src/main/java/org/example/Database.java
@@ -7,9 +7,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String URL = "jdbc:postgresql://localhost:5432/peliculas_db";
+
+    //coneccion a postgresql
+
+    //private static final String URL = "jdbc:postgresql://localhost:5432/peliculas_db";
+   // private static final String USER = "postgres";
+   // private static final String PASSWORD = "Peluche10";
+
+    //coneccion a supabase
+
+    //jdbc:postgresql://db.wxqbzlomnfohdcrmomwg.supabase.co:5432/postgres?user=postgres&password=[YOUR-PASSWORD]
+
+
+    private static final String URL = "jdbc:postgresql://wxqbzlomnfohdcrmomwg.supabase.co:5432/postgres?sslmode=require";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Peluche10";
+    private static final String PASSWORD = "Esponjita10"; // Reemplaza esto con tu contraseña real de Supabase
+
+
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
